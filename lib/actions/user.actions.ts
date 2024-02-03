6c2b821fb75e26e6fe515a918ea90e5e7a0d7e37
kntjhnllc <kentjohnliloc@gmail.com>
2024-02-03 23:33:05 +0800

    fix mongodb problem

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -12,10 +12,12 @@ import { CreateUserParams, UpdateUserParams } from '@/types'
 
 export async function createUser(user: CreateUserParams) {
   try {
-    await connectToDatabase()
+    await connectToDatabase();
 
-    const newUser = await User.create(user)
-    return JSON.parse(JSON.stringify(newUser))
+    const newUser = await User.create(user);
+
+    return JSON.parse(JSON.stringify(newUser));
+
   } catch (error) {
     handleError(error)
   }
@@ -25,10 +27,10 @@ export async function getUserById(userId: string) {
   try {
     await connectToDatabase()
 
-    const user = await User.findById(userId)
+    const user = await User.findById(userId);
 
     if (!user) throw new Error('User not found')
-    return JSON.parse(JSON.stringify(user))
+    return JSON.parse(JSON.stringify(user));
   } catch (error) {
     handleError(error)
   }
@@ -36,12 +38,12 @@ export async function getUserById(userId: string) {
 
 export async function updateUser(clerkId: string, user: UpdateUserParams) {
   try {
-    await connectToDatabase()
+    await connectToDatabase();
 
-    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, { new: true })
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, { new: true });
 
     if (!updatedUser) throw new Error('User update failed')
-    return JSON.parse(JSON.stringify(updatedUser))
+    return JSON.parse(JSON.stringify(updatedUser));
   } catch (error) {
     handleError(error)
   }
