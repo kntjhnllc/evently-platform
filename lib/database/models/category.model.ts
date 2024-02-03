9e25e99584e70b999ec7d02a3e55ccd140126be7
kntjhnllc <kentjohnliloc@gmail.com>
2024-02-03 22:26:02 +0800

    first commit Backend {monggodb}

---
+++ lib/database/models/category.model.ts
@@ -0,0 +1,16 @@
+import exp from "constants";
+import { Document } from "mongodb";
+import { Schema, model, models } from "mongoose";
+
+export interface ICategory extends Document{
+    _Id: string;
+    name: string;
+}
+
+const CategorySchema = new Schema ({
+    name: {type: String, required: true, unique: true},
+})
+
+const Category = models.Category || model('Category', CategorySchema);
+
+export default Category;
