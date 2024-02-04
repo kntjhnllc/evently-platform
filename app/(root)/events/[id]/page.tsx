fc7f15e4d3d70b6d231453c7c96a4d39375a68a2
kntjhnllc <kentjohnliloc@gmail.com>
2024-02-04 19:36:00 +0800

    Related Events

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,16 +1,24 @@
-import { getEventById } from '@/lib/actions/event.actions'
+import Collection from '@/components/shared/Collection';
+import { getEventById, getRelatedEventsByCategory } from '@/lib/actions/event.actions'
 import { formatDateTime } from '@/lib/utils';
 import { SearchParamProps } from '@/types'
 import Image from 'next/image';
 import React from 'react'
 
-const EventDeatils = async ( {params: { id }}: SearchParamProps) => {
+const EventDeatils = async ( {params: { id }, searchParams}: SearchParamProps) => {
     const event = await getEventById(id);
 
+    const relatedEvents = await getRelatedEventsByCategory({
+        categoryId: event.category._id,
+        eventId: event._id,
+        page: searchParams.page as string,
+    })
+
     return (
+        <>
         <section className='flex justify-center bg-primary-50 bg-dotted-pattern bg-contain'
         >   
-            <div className='grid gric-cols-1 md:grid-col-2 2xl:max-w-7xl'>
+            <div className='grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl'>
                 <Image 
                     src={event.imageUrl}
                     alt="hero image" 
@@ -18,7 +26,7 @@ const EventDeatils = async ( {params: { id }}: SearchParamProps) => {
                     height={1000}
                     className='h-full min-h-[300px] object-cover object-center'            
                 />
-                <div className='flex w-full flex-col gap-8 p-5 md:p-10'>
+                <div className='flex w-full flex-col gap-8 p-5 md:p-10 justify-center content-center'>
                     <div className='flex flex-col gap-6'>
                         <h2 className='h2-bold'>{event.title}</h2>
                     
@@ -71,7 +79,22 @@ const EventDeatils = async ( {params: { id }}: SearchParamProps) => {
                 </div>
             </div>
         </section>
-  )
+
+        {/**EVENTS WITH THE SAME CATEGORY */}
+        <section className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
+            <h2 className='h2-bold'>Related Events</h2>
+            <Collection 
+                data={relatedEvents?.data}
+                emptyTitle="No Related Events Found"
+                emptyStateSubtext="Come back later"
+                collectionType="All_Events"
+                limit={6}
+                page={1}
+                totalPages={2}
+            />
+        </section>
+        </>
+    )
 }
 
 export default EventDeatils
