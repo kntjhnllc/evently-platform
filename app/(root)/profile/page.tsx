553a70b4fe725481b2768ade48e5787718ab71d0
kntjhnllc <kentjohnliloc@gmail.com>
2024-02-07 21:30:26 +0800

    FULLY FUNCTIONAL EVENT PLATFORM V1

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -3,13 +3,17 @@ import { Button } from '@/components/ui/button'
 import { getEventsByUser } from '@/lib/actions/event.actions'
 import { getOrdersByUser } from '@/lib/actions/order.actions'
 import { IOrder } from '@/lib/database/models/order.model'
+import { SearchParamProps } from '@/types'
 import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
 
-const ProfilePage = async () => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
     const { sessionClaims } = auth()
     const userId = sessionClaims?.userId as string;
+
+    const ordersPage = Number(searchParams?.ordersPage) || 1;
+    const eventsPage = Number(searchParams?.eventsPage) || 1;
 
     const orders = await getOrdersByUser({ userId, page:1})
 
@@ -35,9 +39,9 @@ const ProfilePage = async () => {
                 emptyStateSubtext="No worries - plenty of exciting events to explore!"
                 collectionType="My_Tickets"
                 limit={3}
-                page={1}
+                page={ordersPage}
                 urlParamName='ordersPage'
-                totalPages={2}
+                totalPages={orders?.totalPages}
             />
         </section>
 
@@ -59,9 +63,9 @@ const ProfilePage = async () => {
                 emptyStateSubtext="Go create some now"
                 collectionType="Events_Organized"
                 limit={6}
-                page={1}
+                page={eventsPage}
                 urlParamName='eventsPage'
-                totalPages={2}
+                totalPages={organizedEvents?.totalPages}
             />
         </section>
     </>
